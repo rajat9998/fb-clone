3f1fec056e4d36d196e65a8115fdba31a3f27356
Rajat Kumar <rajat.kumar9998@gmail.com>
2020-09-20 18:23:01 +0530

    Added Header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAajiE8Dzs2b3vSI0A0U6itT-f0SSn6Jqk",
+    authDomain: "facebook-clone-7c813.firebaseapp.com",
+    databaseURL: "https://facebook-clone-7c813.firebaseio.com",
+    projectId: "facebook-clone-7c813",
+    storageBucket: "facebook-clone-7c813.appspot.com",
+    messagingSenderId: "229893236734",
+    appId: "1:229893236734:web:47c88041a890eb5d4902af",
+    measurementId: "G-W0RX5PKF2Y"
+  };
